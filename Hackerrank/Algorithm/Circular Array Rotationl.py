54478fdc808595781dafce985c773723b9d24286
Chris <122718192+Chrisyeu11@users.noreply.github.com>
2024-06-15 03:01:56 -0500

    Update Circular Array Rotationl.py

--- Hackerrank/Algorithm/Circular Array Rotationl.py
+++ Hackerrank/Algorithm/Circular Array Rotationl.py
@@ -1,3 +1,12 @@
+# Complete the 'circularArrayRotation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER k
+#  3. INTEGER_ARRAY queries
+#
+
 def circularArrayRotation(a, k, queries):
     # Calculate effective rotations needed (handling cases where k is greater than length of array)
     n = len(a)
@@ -10,3 +19,5 @@ def circularArrayRotation(a, k, queries):
     results = [rotated_a[q] for q in queries]
 
     return results
+
+if __name__ == '__main__':
